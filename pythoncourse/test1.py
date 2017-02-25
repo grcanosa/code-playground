5480f16b1a965cf8de61be611c29ee9203dbe7d8
Gonzalo Rodríguez-Canosa <grcanosa@gmail.com>
2017-02-25 21:37:00 +0100

    pyhton course added

---
+++ pythoncourse/test1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import math
+
+def cel2far(cel):
+  return cel*3/5+18;
+
+def eleva(n1,n2):
+  print("Elevando "+str(n1)+" a "+str(n2));
+  return math.pow(n1, n2)
+
+
+from math import pow
+
+
+class Persona:
+  def __init__(self):
+    self.telefono = 0;
+    self.direccion = "";
+
+
+
+
+sara = Persona()
+sara.direccion ="Mi caaasa"
