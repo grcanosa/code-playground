c343ae515b4175b0b19d07adb3a9ce31622b391d
Gonzalo Rodríguez Canosa <grcanosa@gmail.com>
2017-05-30 14:44:01 +0200

    Removed unnecessary info

--- general/grnasPush.sh
+++ general/grnasPush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPONAME="grnas"
-REPOSSH="root@grcanosa.synology.me"
+REPOSSH="root@XXX"
 REPOGENDIR="/volume1/backup/gitproyects/"
 
 
@@ -52,67 +52,3 @@ announceREPO "RTPS"
 addREPO $REPONAME $REPOSSH:$REPOGENDIR"RTPS.git"
 pushREPO $MYPASS
 remoREPO $REPONAME
-
-echo "Entering thirdparty"
-cd thirdparty
-
-announceREPO "IDL"
-cd idl
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"eprosima-common/idl.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-announceREPO "FASTCDR"
-cd fastcdr
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"fastcdr.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-
-announceREPO "COMMON CODE"
-cd eprosima-common-code
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"eprosima-common/code.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-
-announceREPO "DEV ENV"
-cd dev-env
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"eprosima-common/dev-env.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-
-cd .. #RTPS
-cd .. # workspace
-#fastbuffers
-cd idt
-announceREPO "IDT"
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"idt.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-
-#fastbuffers
-cd fastbuffers
-announceREPO "FASTBUFFERS"
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"fastbuffers.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-
-#ddsrecorder
-announceREPO "DDSRECORDER"
-cd ddsrecorder
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"ddsrecorder.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
-
-#ddsrecorder
-announceREPO "BMS"
-cd bms
-addREPO $REPONAME $REPOSSH:$REPOGENDIR"BMS.git"
-pushREPO $MYPASS
-remoREPO $REPONAME
-cd ..
